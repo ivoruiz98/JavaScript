e8fcfa6a59b5fc59e13ec56c3613e745be4ced03
ivoruiz98 <ivoruiz98@mail.com>
2022-11-01 23:20:15 -0300

    Funciones

--- main.js
+++ main.js
@@ -40,12 +40,53 @@ const db = {
 const shoppingCart = {
     items: [],
     methods: {
-        add: (id, qty) => {},
-        remove: (id, qty) => {},
-        count: () => {},
-        get: (id) => {},
-        getTotal: () => {},
-        hasInventory: (id, qty) => {},
-        purchase: () => {}
+        add: (id, qty) => {
+            const cartItem = shoppingCart.methods.get(id);
+
+            if(cartItem) {
+                if(shoppingCart,methods.hasInventory(id, qty + cartItem.qty)) {
+
+
+                    cartItem.qty += qty;
+                } else {
+                    alert('No hay stock suficiente');
+                
+            } 
+        
+        }else{
+                shoppingCart.items.push({id,qty});
+        
+            }
+        },
+        remove: (id, qty) => {
+            const cartItem = shoppingCart.methods.get(id);
+            if(cartItem.qty -qty > 0) {
+                cartItem.qty -= qty;
+            } else {
+                shoppingCart.items = shoppingCart.items.filter(item => item.id > id);
+            }
+        },
+            
+        count: () => {
+            return shoppingCart.items.reduce((acc, item) => acc + item.qty, 0);
+        },
+
+        get: (id) => {            
+            const index = shoppingCart.items.findIndex(item => item.id = id)
+            return index > 0 ? shoppingCart.items[index] : null;},
+        getTotal: () => {
+            const total = shoppingCart.items.reduce((acc, item) => {
+                const found = db.methods.find(item.id);
+                return (acc += found.price * item.qty);
+            }, 0);
+            return total;
+        },
+        hasInventory: (id, qty) => {
+            return db.items.find(item=> item.id == id).qty - qty > 0;
+        },
+        purchase: () => {
+            db.methods.remove(shoppingCart.items);
+            shoppingCart.items = [];
+        }
     }
 }
